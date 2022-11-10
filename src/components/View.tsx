ac46bc48e102bfdb0c5576c14ccf4dd1bd188c44
Robert Graarup Kittilsen <robert.kittilsen@vy.no>
2022-11-10 15:55:08 +0100

    added onSubmit

--- src/components/View.tsx
+++ src/components/View.tsx
@@ -20,22 +20,28 @@ export function View() {
     setInput(e.target.value);
   };
 
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setExample(input);
+  }
+
   return (
     <Container maxW="md">
       <Box p={5} shadow="md" borderWidth="1px" mt={8}>
         <Flex gap={8} direction="column">
           <Code>Example key: {example}</Code>
-          <InputGroup>
-            <Input type="text" value={input} onChange={handleOnChange} />
-            <Button
-              onClick={() => setExample(input)}
-              variant="outline"
-              type="submit"
-              ml={2}
-            >
-              <Text>Submit</Text>
-            </Button>
-          </InputGroup>
+          <form onSubmit={onSubmit}>
+            <InputGroup>
+              <Input type="text" value={input} onChange={handleOnChange} />
+              <Button
+                variant="outline"
+                type="submit"
+                ml={2}
+              >
+                <Text>Submit</Text>
+              </Button>
+            </InputGroup>
+          </form>
         </Flex>
       </Box>
     </Container>
